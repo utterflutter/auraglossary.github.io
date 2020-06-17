105d08eda42bf231c371ca5b973dbb9337dd6e65
utterflutter <66876582+utterflutter@users.noreply.github.com>
2020-06-17 15:48:47 +0100

    Updated

--- scripts.js
+++ scripts.js
@@ -1,99 +1,103 @@
-var grid = [	[0,	0, 0, 0, '1', 0, 0, 0, 0, 0],
-				[0, 0, 0, 0, '1', 0, 0, 0, 0, 0],
-				[0, 0, 0, 0, '1', 0, '2', 0, 0, 0],
-				[0, '3', '3', '3', '3,1', '3', '3,2', '3', '3', '3,4'],
-				[0, 0, 0, 0, 0, 0, '2', 0, 0, '4'],
-				[0, 0, 0, 0, 0, 0, '2', 0, 0, '4'],
-				[0, 0, '5', '5', '5', '5', '2,5', '5', 0, '4'],
-				[0, 0, 0, 0, 0, 0, '2', 0, 0, '4'],
-				[0, 0, 0, 0, 0, 0, '2', 0, 0, 0],
-				['6', '6', '6', '6', '6', '6', '2,6', 0, 0, 0],
-				[0, 0, 0, 0, 0, 0, '2', 0, 0, 0]
+var grid = [	[0,	0, 0, 0, 0, '1', 0, 0, 0, 0, 0,0,0],
+				[0, 0, 0, 0, 0, '1', 0, '2', 0, '3', 0,0,0],
+				[0, 0, 0, '4', 0, '1', 0, '2', 0, '3', 0,0,'5'],
+				[0, 0, 0, '4', 0, '1', 0, '2', 0, '3', 0,0,'5'],
+				[0, 0, 0, '4', 0, '1', 0, '2', 0, '3', 0,0,'5'],
+				[0, '6', '6', '6,4', '6', '6,1', '6', '6,2', '6', '6,3', '6','6','6,5'],
+				[0, 0, 0, '4', 0, '1', 0, '2', 0, '3', 0,0,'5'],
+				[0, 0, 0, '4', 0, '1', 0, '2', 0, '3', 0,0,0],
+				['7', '7', '7', '7,4', '7', '7,1', 0, '2', 0, '3', 0,0,0],
+				[0, 0, 0, '4', 0, '1', 0, '2', 0, 0, 0,0,0],
+                [0, 0, 0, '4', 0, '1', 0, '2', 0, 0, 0,0,0],
+                [0, 0, 0, '4', 0, '1', 0, '2', 0, 0, 0,0,0],
+                [0, 0, 0, 0, 0, 0, 0, '2', 0, 0, 0,0,0]
 			];
 
-var clues = ["Education Hub",
-             "Information Technology Hub",
-             "Cultural Hub",
-             "Capital of India",
-             "India's financial capital",
-             "Saffron region"
+var clues = ["act of setting apart for a particular purpose again",
+             "buried erosional or non-depositional surface separating two rock masses or strata of different agesm indicating that sediment deposition was not continous",
+             "a fence or other abstacle that prevents movement or access",
+             "defamilarization",
+             "relating to a firm, institution, or society",
+             "the things and conditions around a person or thing",
+             "lying dormant or hidden until circumstances are suitable for development or manifestation"
             ];
 
-var answers = ["pune",
-             "bangalore",
-             "hyderabad",
-             "delhi",
-             "mumbai",
-             "kashmir"
+var answers = ["reallocation",
+             "unconformity",
+             "barriers",
+             "ostranenie",
+             "house",
+             "surroundings",
+             "latent"
             ];
 
 //Draw grid
-$.each(grid,function(i){
+$.each(grid,function(i) {
+
     var row = $('<tr></tr>');
-	$.each(this,function(j){
-        if(this == 0){
+
+	$.each(this,function(j) {
+        if (this == 0) {
         	$(row).append('<td class="square empty"></td>');  
-        }
-        else{
+        } else {
             var question_number = String(grid[i][j]).split(",");
             
             var starting_number = '';
             var question_number_span = '';
             
-            for(var k = 0;k < question_number.length;k++){
+            for(var k = 0;k < question_number.length;k++) {
                 var direction = get_direction(question_number[k]);
                 var startpos = get_startpos(question_number[k],direction);
                 
-                if(direction == "horizontal" && startpos[0] == i && startpos[1] == j){
-                    starting_number += question_number[k]+",";
-                                
-                }
-                else if(direction == "vertical" && startpos[0] == j && startpos[1] == i){
+                if(direction == "horizontal" && startpos[0] == i && startpos[1] == j) {
+                    starting_number += question_number[k]+",";               
+                } else if(direction == "vertical" && startpos[0] == j && startpos[1] == i){
                     starting_number += question_number[k]+",";
                 }              
                 
             }
-            if(starting_number != ""){
+            if(starting_number != "") {
                 question_number_span = '<span class="question_number">'+starting_number.replace(/(^,)|(,$)/g, "")+'</span>';   
             }
             
-            $(row).append('<td>'+question_number_span+'<div class="square letter" data-number="'+this+'" contenteditable="true"></div></td>'); 
+            $(row).append('<td class="letter-td">'+question_number_span+'<div class="square letter" data-number="'+this+'" contenteditable="true"></div></td>'); 
         }
     });
-    $("#puzzle").append(row);
+    $("#crossword-inner").append(row);
 });
 
 //Draw hints
 var vertical_hints = $('<div id="vertical_hints"></div>');
 var horizontal_hints = $('<div id="horizontal_hints"></div>');
-$.each(clues,function(index){
+
+$.each(clues,function(index) {
     
     var direction = get_direction(index+1);
     
-    if(direction == "horizontal"){
-        $(horizontal_hints).append('<div class="hint"><b>'+(index+1)+'</b>.-'+clues[index]+'</hint>');
-    }
-    else if(direction == "vertical"){
-    	$(vertical_hints).append('<div class="hint"><b>'+(index+1)+'</b>.-'+clues[index]+'</hint>');
+    if(direction == "horizontal") {
+        $(horizontal_hints).append('<div class="hint">'+(index+1)+'. '+clues[index]+'</hint>');
+    } else if(direction == "vertical") {
+    	$(vertical_hints).append('<div class="hint">'+(index+1)+'. '+clues[index]+'</hint>');
     }
 });
+
 $("#vertical_hints_container").append(vertical_hints);
 $("#horizontal_hints_container").append(horizontal_hints);
 
-$(".letter").keyup(function(){
+$(".letter").keyup(function() {
     var this_text = $(this).text();
     if(this_text.length > 1){
     	$(this).text(this_text.slice(0,1));
     }
 });
 
-$(".letter").click(function(){
+$(".letter").click(function() {
     document.execCommand('selectAll',false,null);
     
     $(".letter").removeClass("active");
     $(this).addClass("active");
     
-    $(".hint").css("color","initial");
+    $(".hint").css("color","#468c4b");
     
     var question_numbers = String($(this).data("number")).split(",");
     
@@ -118,13 +122,6 @@ $("#clear_all").click(function(){
     $.each(question_numbers,function(){
         clearAnswer(this);
     });
-});
-
-$("#check").click(function(){
-    $("#puzzle td div").css("color","initial");
-    for(var i = 0;i < answers.length;i++){
-        checkAnswer(i+1);
-    }    
 });
 
 $("#clue").click(function(){
@@ -173,7 +170,7 @@ function get_startpos(question_number,direction){
 }
     
 function fillAnswer(question_number){
-    $("#puzzle td div").css("color","initial");
+    $("#crossword td div").css("color","#468c4b");
     
     var question_answer = answers[question_number-1];
     var direction = get_direction(question_number);
@@ -182,20 +179,19 @@ function fillAnswer(question_number){
     
     if(direction == "horizontal"){
         for(var i = 0; i < answer_letters.length; i++){
-            $("#puzzle tr:nth-child("+(startpos[0]+1)+") td:nth-child("+(startpos[1]+1+i)+") div").text(answer_letters[i]);
+            $("#crossword tr:nth-child("+(startpos[0]+1)+") td:nth-child("+(startpos[1]+1+i)+") div").text(answer_letters[i]);
         }
     	 
-    }
-    else if(direction == "vertical"){
+    } else if(direction == "vertical"){
         for(var i = 0; i < answer_letters.length; i++){
-            $("#puzzle tr:nth-child("+(startpos[1]+1+i)+") td:nth-child("+(startpos[0]+1)+") div").text(answer_letters[i]);
+            $("#crossword tr:nth-child("+(startpos[1]+1+i)+") td:nth-child("+(startpos[0]+1)+") div").text(answer_letters[i]);
         }
     	 
     }
 }
 
 function clearAnswer(question_number){
-    $("#puzzle td div").css("color","initial");
+    $("#crossword td div").css("color","#468c4b");
     
     var question_answer = answers[question_number-1];
     var direction = get_direction(question_number);
@@ -204,37 +200,12 @@ function clearAnswer(question_number){
     
     if(direction == "horizontal"){
         for(var i = 0; i < answer_letters.length; i++){
-            $("#puzzle tr:nth-child("+(startpos[0]+1)+") td:nth-child("+(startpos[1]+1+i)+") div").text('');
+            $("#crossword tr:nth-child("+(startpos[0]+1)+") td:nth-child("+(startpos[1]+1+i)+") div").text('');
         }
     	 
-    }
-    else if(direction == "vertical"){
+    } else if(direction == "vertical"){
         for(var i = 0; i < answer_letters.length; i++){
-            $("#puzzle tr:nth-child("+(startpos[1]+1+i)+") td:nth-child("+(startpos[0]+1)+") div").text('');
-        }
-    	 
-    }
-}
-
-function checkAnswer(question_number){
-    var question_answer = answers[question_number-1];
-    var direction = get_direction(question_number);
-    var startpos = get_startpos(question_number,direction);
-    var answer_letters = question_answer.split("");
-    
-    if(direction == "horizontal"){
-        for(var i = 0; i < answer_letters.length; i++){
-            if($("#puzzle tr:nth-child("+(startpos[0]+1)+") td:nth-child("+(startpos[1]+1+i)+") div").text() != question_answer[i] && $("#puzzle tr:nth-child("+(startpos[0]+1)+") td:nth-child("+(startpos[1]+1+i)+") div").text() != ""){
-                $("#puzzle tr:nth-child("+(startpos[0]+1)+") td:nth-child("+(startpos[1]+1+i)+") div").css("color","red");
-            }
-        }
-    	 
-    }
-    else if(direction == "vertical"){
-        for(var i = 0; i < answer_letters.length; i++){
-            if($("#puzzle tr:nth-child("+(startpos[1]+1+i)+") td:nth-child("+(startpos[0]+1)+") div").text() != question_answer[i] && $("#puzzle tr:nth-child("+(startpos[1]+1+i)+") td:nth-child("+(startpos[0]+1)+") div").text() != ""){
-                $("#puzzle tr:nth-child("+(startpos[1]+1+i)+") td:nth-child("+(startpos[0]+1)+") div").css("color","red");
-            }
+            $("#crossword tr:nth-child("+(startpos[1]+1+i)+") td:nth-child("+(startpos[0]+1)+") div").text('');
         }
     	 
     }
@@ -247,13 +218,11 @@ function showClue(question_number,i,j){
     var answer_letters = question_answer.split("");
     
     if(direction == "horizontal"){
-        $("#puzzle tr:nth-child("+(j+1)+") td:nth-child("+(i+1)+") div").text(answer_letters[i - startpos[1]]).css("color","initial");
-    }
-    else if(direction == "vertical"){
-        $("#puzzle tr:nth-child("+(j+1)+") td:nth-child("+(i+1)+") div").text(answer_letters[j - startpos[1]]).css("color","initial");
+        $("#crossword tr:nth-child("+(j+1)+") td:nth-child("+(i+1)+") div").text(answer_letters[i - startpos[1]]).css("color","#468c4b");
+    } else if(direction == "vertical"){
+        $("#crossword tr:nth-child("+(j+1)+") td:nth-child("+(i+1)+") div").text(answer_letters[j - startpos[1]]).css("color","#468c4b");
     }
 }
-
 
 
 
